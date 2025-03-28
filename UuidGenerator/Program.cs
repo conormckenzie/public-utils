71d09af8af4ac2caa7efe4cda2061d235d1cc7cb
Conor McKenzie <conor.mckenzie314@protonmail.com>
2025-03-28 17:05:51 -0400

    Add UUID v4 generator

---
+++ UuidGenerator/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Guid randomUuid = Guid.NewGuid();
+        Console.WriteLine($"Generated UUID v4: {randomUuid}");
+    }
+}
